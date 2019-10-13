5ce26ea233e21c6a3521181982d1a0f131cb8e7f
dbsitt <carloginocatapang@gmail.com>
2019-10-13 20:45:58 +0800

    Display error on http error

--- src/app/interceptors.ts
+++ src/app/interceptors.ts
@@ -4,18 +4,38 @@ import {
   HttpInterceptor,
   HttpHandler,
   HttpRequest,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { catchError } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
+  constructor(private snackBar: MatSnackBar) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const { api } = environment;
     const apiReq = req.clone({ url: `${api}/${req.url}` });
-    return next.handle(apiReq);
+    return next.handle(apiReq).pipe(
+      catchError((err: HttpErrorResponse) => {
+        let message = `Status ${err.status}. ${err.statusText}`;
+        if (err.status === 0) {
+          message = `Endpoint '${api}/${req.url}' not found`;
+        } else if (err.status === 404) {
+          message = `404 Error`;
+        } else if (err.status === 500) {
+          message = '500 Server Error';
+        }
+        this.snackBar.open(message, 'Close', {
+          duration: 4000,
+        });
+        return throwError(err);
+      })
+    );
   }
 }
