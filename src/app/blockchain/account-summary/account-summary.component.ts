375f8c4cdfcacc04caff46a47d80a443ac2e662d
tuan <nguyenminhtuanfit@gmail.com>
2019-10-17 02:34:20 +0800

    add account

--- src/app/blockchain/account-summary/account-summary.component.ts
+++ src/app/blockchain/account-summary/account-summary.component.ts
@@ -44,28 +44,35 @@ export class AccountSummaryComponent implements OnInit {
   get displayedColumns() {
     return [
       'holder',
-      'walletRef',
-      'accountNumber',
       'accountName',
+      'accountNumber',
+      'productName',
+      'source',
+      'quantity',
       'currency',
       'amount',
+      'positionStatus',
     ];
   }
+
   get currentUserRole() {
     return this.helperService.getCurrentUserRole();
   }
   fetchAccountSummary() {
     this.columns = [
       'holder',
-      'walletRef',
-      'accountNumber',
       'accountName',
+      'accountNumber',
+      'productName',
+      'source',
+      'quantity',
       'currency',
       'amount',
+      'positionStatus',
     ];
     this.uiStore.dispatch(setLoading({ value: true }));
     this.httpClient
-      .get(this.helperService.getBaseUrl() + '/getAccounts')
+      .get(this.helperService.getBaseUrl() + '/portfolio')
       .pipe(
         finalize(() => {
           this.uiStore.dispatch(setLoading({ value: false }));
@@ -74,7 +81,53 @@ export class AccountSummaryComponent implements OnInit {
       .subscribe(
         (response: any) => {
           if (response !== null) {
-            this.tableData = response;
+            for (let i = 0; i < response.length; i++) {
+              let productIdentifier =
+                response[i].portfolio.aggregationParameters.product[0].security
+                  .bond.productIdentifier;
+              let source = productIdentifier.source;
+              let productName = productIdentifier.identifier[0].value;
+              let party = response[i].portfolio.aggregationParameters.party[0];
+              let acc = party.value;
+              let accountName = acc.account.accountName.value;
+              let accountNumber = acc.account.accountNumber.value;
+              let holder = party.value.name.value;
+
+              let position = response[i].portfolio.portfolioState.positions[0];
+              let quantity = position.cashBalance.amount;
+              let currency = position.cashBalance.currency.value;
+
+              let amount = position.quantity.amount;
+              let positionStatus = position.positionStatus;
+
+              console.log('source: ' + source);
+              console.log(productName);
+              console.log(accountName);
+              console.log(accountNumber);
+              console.log(quantity);
+              console.log(currency);
+              console.log(amount);
+              console.log(positionStatus);
+
+              // alert(response[i].portfolio.product[0].security.bond.productIdentifier.identifier)
+              console.log(response[i]);
+              var _account = new Object();
+              _account['source'] = source;
+              _account['productName'] = productName;
+              _account['accountName'] = accountName;
+              _account['accountNumber'] = accountNumber;
+              _account['quantity'] = quantity;
+              _account['currency'] = currency;
+              _account['amount'] = amount;
+              _account['positionStatus'] = positionStatus;
+              _account['holder'] = holder;
+
+              console.log(_account);
+
+              this.tableData.push(_account);
+            }
+
+            //this.tableData = response;
           }
         },
         () => {
@@ -93,6 +146,20 @@ export class AccountSummaryComponent implements OnInit {
     this.account$ = this.store.pipe(select(getCurrentUser));
     this.helperService.currentUser$.subscribe(e => {
       console.log('calling fetch account summary');
+
+      //_account.
+
+      // accountHolder: string;
+      // accountName: string;
+      // accountNumber: string;
+      // product: string;
+      // source: string;
+      // quantity: string;
+      // currency: string;
+      // amount: string;
+      // positionStatus: string;
+
+      //)
       this.fetchAccountSummary();
     });
 
@@ -120,3 +187,14 @@ export class AccountSummaryComponent implements OnInit {
     // );
   }
 }
+// interface AccountDetail {
+//   accountHolder: string;
+//   accountName: string;
+//   accountNumber: string;
+//   product: string;
+//   source: string;
+//   quantity: string;
+//   currency: string;
+//   amount: string;
+//   positionStatus: string;
+// }
