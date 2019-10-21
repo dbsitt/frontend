54e1d6ed2f832828194a3b3254c8b859ebf54d6a
dbsitt <carloginocatapang@gmail.com>
2019-10-22 07:42:26 +0800

    Will navigate away from allocate trade if not a broker

--- src/app/blockchain/allocate-trade/allocate-trade.component.ts
+++ src/app/blockchain/allocate-trade/allocate-trade.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnDestroy,
+} from '@angular/core';
 import { cloneDeep } from 'lodash';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material';
 import { HelperService } from '../helperService';
+import { ROLES } from '../blockchain.constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-allocate-trade',
   templateUrl: './allocate-trade.component.html',
   styleUrls: ['./allocate-trade.component.scss'],
 })
-export class AllocateTradeComponent implements OnInit {
+export class AllocateTradeComponent implements OnInit, OnDestroy {
   columns = ['tradeAndClient', 'productRelated', 'valueRelated'];
 
   @Input() data = null;
@@ -18,11 +27,13 @@ export class AllocateTradeComponent implements OnInit {
 
   allocation1 = 0;
   allocation2 = 0;
+  currentUserSubscription$: any;
 
   constructor(
     private httpClient: HttpClient,
     private snackBar: MatSnackBar,
-    private helperService: HelperService
+    private helperService: HelperService,
+    private router: Router
   ) {}
 
   onValueChange(type, event) {
@@ -40,6 +51,17 @@ export class AllocateTradeComponent implements OnInit {
     if (this.data) {
       this.tableData = cloneDeep([this.data]);
     }
+    this.currentUserSubscription$ = this.helperService.currentUser$.subscribe(
+      e => {
+        if (e.role !== ROLES.BROKER) {
+          this.router.navigateByUrl('transactions/account');
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription$.unsubscribe();
   }
 
   validate(): boolean {
@@ -51,20 +73,23 @@ export class AllocateTradeComponent implements OnInit {
       this.snackBar.open('Should be a positive integer', 'Close', {
         duration: 2000,
       });
+      return false;
     }
     const total: number = +this.allocation1 + +this.allocation2;
-    console.log(total);
-    console.log(this.allocation1);
-    console.log(this.allocation2);
-    console.log(quantity);
-    console.log(quantity === total);
-    console.log(typeof +quantity, typeof total);
-    return total === +quantity;
+    if (total !== +quantity) {
+      this.snackBar.open(
+        'Allocated total should be equal to transaction quantity',
+        'Close',
+        {
+          duration: 2000,
+        }
+      );
+      return false;
+    }
+    return true;
   }
 
   isValidNumber(num): boolean {
-    console.log(num);
-    console.log(!isNaN(+num));
     return !isNaN(+num) && num > 0;
   }
 
